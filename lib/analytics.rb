56b1266d7b9d4c1f465544ec933a8796f1b66af4
khoainx <khoai.nx@gmail.com>
2018-10-19 05:50:06 +0700

    Answer #3.5

--- lib/analytics.rb
+++ lib/analytics.rb
@@ -3,9 +3,19 @@ module Analytics
     Analytic.create(event: data[0], value: data[1], event_time: data[2] || Time.now)
   end
 
-  def self.events(data)
-    data.each do |sub_data|
-      Analytic.create(event: sub_data[0], value: sub_data[1], event_time: sub_data[2] || Time.now)
-    end
+  def self.events
+    source = Queue.new
+    datas = (1..50)
+    datas.each { |data| source << self.event([[0, 1].sample, data, Time.now]) }
+
+    (1..23).to_a.map do
+      Thread.new do
+        until source.empty?
+          item = source.pop
+          sleep 0.5
+          puts "Processed: #{item} at #{Time.now}"
+        end
+      end
+    end.map(&:join)
   end
 end
