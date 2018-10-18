564fda2f34d54a93e5b432aa79c4f82493688494
khoainx <khoai.nx@gmail.com>
2018-10-19 04:11:38 +0700

    Answer #1.2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,7 @@ class BooksController < ApplicationController
   # GET /books/new
   def new
     @book = Book.new
+    @book.build_author
   end
 
   # GET /books/1/edit
@@ -69,6 +70,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :isbn)
+      params.require(:book).permit(:title, :isbn, author_attributes: [:name])
     end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :author, optional: true
+
+  accepts_nested_attributes_for :author
 end
