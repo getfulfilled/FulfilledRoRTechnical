f591b6df7ffee374876117eae9c5d202a185ca3b
khoainx <khoai.nx@gmail.com>
2018-10-19 05:08:19 +0700

    Answer #3.4

--- lib/analytics.rb
+++ lib/analytics.rb
@@ -2,4 +2,10 @@ module Analytics
   def self.event(data)
     Analytic.create(event: data[0], value: data[1], event_time: data[2] || Time.now)
   end
+
+  def self.events(data)
+    data.each do |sub_data|
+      Analytic.create(event: sub_data[0], value: sub_data[1], event_time: sub_data[2] || Time.now)
+    end
+  end
 end
